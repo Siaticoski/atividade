e9db90073ec3564cec1be3010d7cbed46286cc27
Siaticoski <caiosiaticoski1@gmail.com>
2025-09-08 17:35:43 -0400

    dasd

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Lista de Compras</Text>
+      <View style={styles.centro}>
+        <View style={styles.base}>
+            <TextInput style={styles.digite}>digite o produto</TextInput>
+            <TouchableOpacity style={styles.plus}>
+            <AntDesign  name="pluscircleo" size={40} color="#9400D3" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +22,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9400D3',
     alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: 'left',
+    flexDirection: 'column',
+    paddingTop: 50,
   },
+  texto:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    
+  },
+  digite: {
+    backgroundColor: 'black',
+    color: 'white',
+    width: '90%',
+    borderRadius: 10,
+    fontSize: 25,
+  },
+  plus: {
+    backgroundColor:'black',
+    width: 55,
+    height: 55,
+    alignItems:'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  centro:{
+    width: '80%',
+    textAlign: 'center',
+    gap: 20,
+  },
+  base: {
+    flexDirection: 'row',
+    width: '100%',
+    gap: 10,
+  },
+  
 });
