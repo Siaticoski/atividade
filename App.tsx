940211471ac1438d639ee665795ff903fbe459ac
Siaticoski <caiosiaticoski1@gmail.com>
2025-09-10 15:56:44 -0400

    dasdas

--- App.tsx
+++ App.tsx
@@ -1,18 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, ListRenderItem, FlatList} from 'react-native';
+import { useState } from 'react';
+type Produto = {
+  id: string,
+  nome: string,
+  comprado: boolean,
+};
+
+
+
 
 export default function App() {
+  const [produto, setProduto] = useState<string>('');
+  const [lista, setLista] = useState<Produto[]>([
+    {id: '1', nome: 'Maça', comprado: true},
+    {id: '2', nome: 'Banana', comprado: true},
+    {id: '3', nome: 'Melancia', comprado:true},
+  ]);
+
+  const adicionarProduto = () => {
+    if (produto.trim() === '') return; {
+      
+      const novoProduto: Produto = {
+        id: Date.now().toString(),
+        nome: produto,
+        comprado: false,
+      };
+
+      setLista([...lista, novoProduto]);
+      setProduto('');
+    };
+  }
+
+  const alternarProduto = (id: string) => {
+    const novaLista = lista.map((item) => 
+      item.id === id ? { ...item, comprado: !item.comprado} : item
+    );
+    setLista(novaLista);
+      
+  }
+
+  const rederItem: ListRenderItem<Produto> = ({item}) => (
+    <TouchableOpacity style={styles.alinhar} onPress={() => alternarProduto(item.id)}>
+      <AntDesign name="checkcircle" size={30} color="black" />
+      <Text style = {styles.produtos}>{item.nome}</Text>
+    </TouchableOpacity>
+
+  )
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Lista de Compras</Text>
+      
       <View style={styles.centro}>
+        <Text style={styles.texto}>Lista de Compras</Text>
         <View style={styles.base}>
-            <TextInput style={styles.digite}>digite o produto</TextInput>
-            <TouchableOpacity style={styles.plus}>
+          <TextInput
+            style={styles.digite}
+            placeholder='Digite o Produto'
+            placeholderTextColor="#FFFFFF"
+            value={produto}
+            onChangeText={setProduto}
+          />
+          <TouchableOpacity style={styles.plus} onPress={adicionarProduto}>
             <AntDesign  name="pluscircleo" size={40} color="#9400D3" />
           </TouchableOpacity>
         </View>
+      </View>
+
+      <View style={styles.lista}>
+        <FlatList
+          data={lista}
+          renderItem={rederItem}
+          keyExtractor={(item) => item.id}
+        />
       </View>
       
     </View>
@@ -24,9 +85,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#9400D3',
     alignItems: 'center',
-    textAlign: 'left',
+    textAlign: 'center',
     flexDirection: 'column',
     paddingTop: 50,
+    gap: 10
   },
   texto:{
     fontSize: 30,
@@ -36,7 +98,7 @@ const styles = StyleSheet.create({
   digite: {
     backgroundColor: 'black',
     color: 'white',
-    width: '90%',
+    width: '83%',
     borderRadius: 10,
     fontSize: 25,
   },
@@ -49,14 +111,37 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   centro:{
-    width: '80%',
-    textAlign: 'center',
-    gap: 20,
+    width: '95%',
+    alignItems: 'flex-start',
+    gap: 10,
   },
   base: {
     flexDirection: 'row',
     width: '100%',
     gap: 10,
   },
+  lista:{
+    borderColor: 'black',
+    alignItems: 'center',
+    marginTop: 20,
+    borderWidth: 2,
+    borderBlockColor: 'black',
+    borderRadius: 10,
+    height: '80%',
+    padding: 10
+  },
+  alinhar:{
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    width: 370,
+    paddingLeft: 10
+  },
+  produtos:{
+    color: 'white',
+    fontSize: 30,
+  }
   
 });
